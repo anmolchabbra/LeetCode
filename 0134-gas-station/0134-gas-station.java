d9e50aa5d25d16c370a378d25dfff5a69330af92
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2023-09-25 17:30:15 -0400

    Time: 2 ms (92.55%), Space: 55.2 MB (54.78%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+       int totalGas = 0, totalCost = 0;
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+        if (totalGas < totalCost) {
+            return -1;
+        }
+        int total = 0, index = 0;
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+            if (total < 0) {
+                total = 0;
+                index = i + 1;
+            }
+        }
+        return index;
+    }
+}
