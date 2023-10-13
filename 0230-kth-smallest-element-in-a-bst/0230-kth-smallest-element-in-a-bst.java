e0031f66452d58036cb0c3e1dadbeeaf0da38a43
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2023-10-13 09:41:11 -0400

    Time: 0 ms (100.00%), Space: 43.7 MB (85.96%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -17,18 +17,18 @@ class Solution {
     int kSmallest = 0;
     int c = 0;
     public int kthSmallest(TreeNode root, int k) {
-        pre(root, k);
+        inOrder(root, k);
         return kSmallest;
     }
-    public void pre(TreeNode root,int k) {
+    public void inOrder(TreeNode root,int k) {
         if (root == null) {
             return;
         }
-        pre(root.left, k);
+        inOrder(root.left, k);
         if (++c == k) {
             kSmallest = root.val;
             return;
         }
-        pre(root.right, k);
+        inOrder(root.right, k);
     }
 }
