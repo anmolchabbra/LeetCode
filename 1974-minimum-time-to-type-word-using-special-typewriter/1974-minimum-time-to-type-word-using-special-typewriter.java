1fe34213db224fd45981d3d0c659ee59c22c9b2b
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2023-12-07 18:36:07 -0500

    Time: 1 ms (70.85%), Space: 40.6 MB (27.21%) - LeetHub

--- 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.java
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.java
@@ -10,16 +10,13 @@ class Solution {
                 n++;
             } else {
                 int far = (int)(curr - onWheel);
-                System.out.println("Far: "+ far);
                 far = Math.abs(far);
                 int clockWise = far;
                 int antiClockWise = (26 - far);
                 int timeToAdd = Math.min(clockWise, antiClockWise);
-                System.out.println("to add: "+ timeToAdd);
                 time += timeToAdd;
                 onWheel = curr;
             }
-            System.out.println("Time is: "+ time);
         }
         return time;
     }
