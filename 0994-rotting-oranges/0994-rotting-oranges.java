9e7a11353c0242d5889d493915bde0b0276d8370
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-02-15 15:06:24 -0500

    Time: 2 ms (76.24%), Space: 42.4 MB (51.88%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,56 @@
+class Solution {
+    
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
+        int countFresh = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new int[] {i, j});
+                } else if (grid[i][j] == 1) {
+                    countFresh++;
+                }
+            }
+        }
+        
+        if (countFresh == 0) return 0;
+        
+        int time = 0;
+        
+        while (!q.isEmpty()) {
+            int size = q.size();
+            
+            for (int i = 0; i < size; i++)  {
+                int[] rottenPos = q.remove();
+                for (int[] dir : dirs ) {
+                    int row = rottenPos[0] + dir[0];
+                    int col = rottenPos[1] + dir[1];
+                    if (row >= 0 && row < grid.length && col >= 0 
+                        && col < grid[0].length && grid[row][col] == 1) {
+                        
+                        
+                        grid[row][col] = 2;
+                        
+                        q.add(new int[] {row, col});
+                    }
+                }
+            }
+            
+            time++;
+        }
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j <  grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return time - 1;
+        
+    }
+}
