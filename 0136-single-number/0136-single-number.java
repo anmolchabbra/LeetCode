cefe9d2f25cb6dc51032ce3a80d06fbb66dc765d
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-06-21 00:38:57 -0400

    Time: 1 ms (99.92%), Space: 45.8 MB (62.38%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor = nums[0];
-        for (int i = 1; i < nums.length; i++) {
+        int xor = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
             xor ^= nums[i];
         }
+        
         return xor;
     }
 }
