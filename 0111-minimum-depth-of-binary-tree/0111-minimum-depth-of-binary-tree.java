6ae3411e9b5bace4de05c0b2eb8024e85fac6065
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-06-22 19:56:03 -0400

    Time: 2 ms (91.53%), Space: 63.3 MB (36.99%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -14,16 +14,58 @@
  * }
  */
 class Solution {
+
+    //DFS Solution
     
-    public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        if (root.right == null) {
-            return 1 + minDepth(root.left);
-        } else if (root.left == null) {
-            return 1 + minDepth(root.right);
-        }
+//     public int minDepth(TreeNode root) {
+//         if (root == null) return 0;
+//         if (root.right == null) {
+//             return 1 + minDepth(root.left);
+//         } else if (root.left == null) {
+//             return 1 + minDepth(root.right);
+//         }
         
-        return 1 +  Math.min(minDepth(root.left), minDepth(root.right));
+//         return 1 +  Math.min(minDepth(root.left), minDepth(root.right));
+        
+//     }
+    
+    //BFS Solution
+     public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+     
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+         
+        q.add(root);
+         
+        int depth = 1;
+         
+         while (!q.isEmpty()) {
+             
+             int size = q.size();
+             
+             while (size > 0) {
+                 
+                  size--;
+                 
+                 TreeNode get = q.poll();
+                 
+                 if (get == null) continue;
+                 
+                 if (get.left == null && get.right == null) return depth;
+                 
+                 
+                 q.add(get.left);
+                 
+                 q.add(get.right);
+                 
+                
+             }
+             depth++;
+             
+         }
+         
+         return -1;
         
     }
 
