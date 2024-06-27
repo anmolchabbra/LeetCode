68800ccdaed6530326db713c3b89dcd218f5cc0c
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-06-26 23:24:26 -0400

    Time: 1 ms (99.67%), Space: 58.3 MB (7.63%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,40 +1,40 @@
-// class Solution {
-//     public int minSubArrayLen(int target, int[] nums) {
-//         int j = 0, i = 0;
-//         int sum = 0;
-//         int minLen = nums.length;
-//         while (j < nums.length) {
-//             sum += nums[j];
-//             if (sum >= target) {
-//                 minLen = Math.min(minLen, (j - i + 1));
-//                 while (sum > target && i <= j) {
-//                     sum -= nums[i];
-//                     i++;
-//                 }
-//                 continue;
-//             } 
-//             j++;
-//         }
-//          return minLen;
-//      //   if (sum < target) return 0;
-       
-//     }
-// }
-
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left = 0, right = 0, sumOfCurrentWindow = 0;
-        int res = Integer.MAX_VALUE;
-
-        for(right = 0; right < nums.length; right++) {
-            sumOfCurrentWindow += nums[right];
-
-            while (sumOfCurrentWindow >= target) {
-                res = Math.min(res, right - left + 1);
-                sumOfCurrentWindow -= nums[left++];
-            }
+        int j = 0, i = 0;
+        int sum = 0;
+        int minLen = nums.length + 1;
+        while (j < nums.length) {
+            sum += nums[j];
+            if (sum >= target) {
+                
+                while (sum >= target && i <= j) {
+                    minLen = Math.min(minLen, (j - i + 1));
+                    sum -= nums[i];
+                    i++;
+                }
+            } 
+            j++;
         }
-
-        return res == Integer.MAX_VALUE ? 0 : res;
+         return minLen == nums.length + 1 ? 0 : minLen;
+     //   if (sum < target) return 0;
+       
     }
 }
+
+// class Solution {
+//     public int minSubArrayLen(int target, int[] nums) {
+//         int left = 0, right = 0, sumOfCurrentWindow = 0;
+//         int res = Integer.MAX_VALUE;
+
+//         for(right = 0; right < nums.length; right++) {
+//             sumOfCurrentWindow += nums[right];
+
+//             while (sumOfCurrentWindow >= target) {
+//                 res = Math.min(res, right - left + 1);
+//                 sumOfCurrentWindow -= nums[left++];
+//             }
+//         }
+
+//         return res == Integer.MAX_VALUE ? 0 : res;
+//     }
+// }
