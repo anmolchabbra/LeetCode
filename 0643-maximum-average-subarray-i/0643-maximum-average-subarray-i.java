f90deaff248963736074c9f3e8a032260969c985
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-06-26 22:45:36 -0400

    Time: 5 ms (40.99%), Space: 56.5 MB (14.24%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -5,22 +5,21 @@ class Solution {
         }
         int i = 0;
         int j = 0;
-        int sum = nums[0];
+        int sum = 0;
         double maxAvg = Integer.MIN_VALUE;
-        while (j < nums.length - 1) {
-            if ((j - i + 1) == k) {
-                double cal = (double)sum / k;
-                System.out.println("Sum: " + sum + "Avg" + cal);
-                maxAvg = Math.max(maxAvg, cal);
+        while (j < nums.length) {
+            
+            sum+= nums[j]; 
+            
+            if ((j - i + 1) >= k) {
+                maxAvg = Math.max(maxAvg, sum);
                 sum -= nums[i];
                 i++;
             }
+            
+             
             j++;
-            sum+= nums[j];  
         }
-        double cal = (double)sum / k;
-            System.out.println("Sum: " + sum + "Avg" + cal);
-                maxAvg = Math.max(maxAvg, cal);
-        return maxAvg;
+        return (double)maxAvg / k;
     }
 }
