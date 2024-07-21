9fe5852cd6d37f48cafb2b0c819c72c03bc91536
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-07-21 15:53:21 -0400

    Time: 0 ms (100.00%), Space: 42.4 MB (50.56%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,22 +10,17 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //make head and make then make head and tail point to it
         ListNode head = new ListNode();
         ListNode tail = head;
         while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
-                if (head == null) {
-                    head = list1;
-                    tail = list1;
-                } 
+                
                 tail.next = list1;
                 list1 = list1.next;
                 
             } else {
-                if (head == null) {
-                    head = list2;
-                    tail = list2;
-                }
+                
                 tail.next = list2;
                 list2 = list2.next;
             }
