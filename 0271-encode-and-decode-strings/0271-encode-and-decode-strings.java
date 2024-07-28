1ebf15fa8ca8f9955ca141aab6d683a16c5236c7
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-07-28 11:47:49 -0400

    Time: 54 ms (5.03%), Space: 45.9 MB (6.72%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -0,0 +1,52 @@
+public class Codec {
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            int len = s.length();
+            sb.append(len);  // Append the length of the string
+            sb.append('#');  // Append the separator
+            sb.append(s);    // Append the actual string
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> decodedList = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            String lenPart = "";
+            // Gather the length of the next string
+            while (str.charAt(i) != '#') {
+                lenPart += str.charAt(i);
+                i++;
+            }
+            // Skip the '#'
+            i++;
+            System.out.println("/Len: " + lenPart);
+
+            // Parse the length of the string
+            int len = Integer.parseInt(lenPart);
+
+            // Extract the string of length 'len'
+            String toAdd = str.substring(i, i + len);
+            System.out.print(toAdd);
+
+            // Add the string to the decoded list
+            decodedList.add(toAdd);
+
+            // Move the index past the extracted string
+            i += len;
+        }
+        return decodedList;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
+
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
