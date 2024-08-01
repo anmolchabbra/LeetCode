317518a94b3290dc69362f0b4bfda7c3dec2960e
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-07-31 23:31:16 -0400

    Time: 7 ms (79.76%), Space: 46.1 MB (27.20%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,19 +1,39 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a ,b) -> Integer.compare(a[0], b[0]));
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int endLast = intervals[0][1];
-        pq.add(endLast);
-        int minRoom = 1;
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] < pq.peek()) {
-                minRoom++;
-                pq.add(intervals[i][1]);
-                continue;
-            }
-            pq.poll();
-            pq.add(intervals[i][1]);
-        }
-        return minRoom;
+        // Arrays.sort(intervals, (a ,b) -> Integer.compare(a[0], b[0]));
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // int endLast = intervals[0][1];
+        // pq.add(endLast);
+        // int minRoom = 1;
+        // for (int i = 1; i < intervals.length; i++) {
+        //     if (intervals[i][0] < pq.peek()) {
+        //         minRoom++;
+        //         pq.add(intervals[i][1]);
+        //         continue;
+        //     }
+        //     pq.poll();
+        //     pq.add(intervals[i][1]);
+        // }
+        // return minRoom;
+            Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        minHeap.add(intervals[0][1]);
+        
+    for (int i = 1; i < intervals.length; i++) {
+        if (!minHeap.isEmpty() && intervals[i][0] >= minHeap.peek() ) {
+          minHeap.poll(); 
+            
+        }    
+        
+        minHeap.add(intervals[i][1]);
+    
+    }
+        
+        
+    return minHeap.size();
+        
     }
 }
