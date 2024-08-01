5f3b796985132f832e36fb83b28263ebbe02949b
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-07-31 23:30:29 -0400

    Time: 8 ms (64.59%), Space: 46 MB (38.09%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,76 +1,19 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-//         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
-       
-//         int[] prev = intervals[0];
-//         int[] rooms = new int[intervals.length];
-//         int curr = 0;
-//         int room = 1;
-//         rooms[0] = intervals[0][1];
-//         for (int i = 1; i < intervals.length; i++) {
-//              prev = intervals[i - 1];
-//             int start = intervals[i][0];
-//             int end = intervals[i][1];
-//             if (start < prev[1]) {
-//                 for (int j = 0 ; j <= curr; j++) {
-//                     if (start >= rooms[j] ) {
-//                         break;
-//                     }
-//                 }
-//                 rooms[++curr] = end;
-//                 for(int k = 0; k <= curr; k++) {
-//                     System.out.println (rooms[k]);
-//                 }
-//                 room++;
-//             }
-//         }
-//         return room;
-        
-//         //Using Number Line approach
-        
-//         int[] start = new int[intervals.length];
-//         int[] end = new int[intervals.length];
-        
-//         for (int i = 0; i < intervals.length; i++) {
-//             start[i] = intervals[i][0];
-//             end[i] = intervals[i][1];
-//         }
-        
-//         Arrays.sort(start);
-//         Arrays.sort(end);
-        
-//         int meetingRooms = 0;
-        
-//         for (int iStart = 0, iEnd = 0; iStart < intervals.length; iStart++) {
-//             if (start[iStart] < end[iEnd]) meetingRooms++;
-//             else iEnd++;
-//         }
-        
-//         return meetingRooms;
-//     }
-        
-    
-    //Using Min heap
-    
-    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
-        minHeap.add(intervals[0][1]);
-        
-    for (int i = 1; i < intervals.length; i++) {
-        if (!minHeap.isEmpty() && intervals[i][0] >= minHeap.peek() ) {
-          minHeap.poll(); 
-            
-        }    
-        
-        minHeap.add(intervals[i][1]);
-    
-    }
-        
-        
-    return minHeap.size();
-        
+        Arrays.sort(intervals, (a ,b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int endLast = intervals[0][1];
+        pq.add(endLast);
+        int minRoom = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < pq.peek()) {
+                minRoom++;
+                pq.add(intervals[i][1]);
+                continue;
+            }
+            pq.poll();
+            pq.add(intervals[i][1]);
+        }
+        return minRoom;
     }
 }
