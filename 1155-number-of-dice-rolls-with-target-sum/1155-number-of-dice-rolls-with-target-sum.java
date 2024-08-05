0bb47a71d2857816f0c3b9c775bc320714daa148
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-08-05 04:33:00 -0400

    Time: 9 ms (92.22%), Space: 41.7 MB (74.19%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,41 +1,30 @@
 class Solution {
-    //int res = 0;
-    double MODULO = 1e9 + 7;
+    long modulo = 1000000007;
     
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp = new int[n + 1][target + 1];
-
         for (int i = 0; i <= n; i++) {
             Arrays.fill(dp[i], -1);
         }
-        helper(n, k, target, dp);
-       return dp[n][target];
+        return rolls(n, k, target, dp);
     }
     
-    private int helper(int n, int k, int target, int[][] dp) {
-        if (n < 0 || target < 0) {
+    private int rolls(int n, int k, int target, int[][] dp) {
+        if (target < 0) {
             return 0;
         }
-        if (n <= 0 && target > 0) {
-            return 0;
+        if (n == 0) {
+            return target == 0 ? 1 : 0;
         }
-        if (n > 0 && target <= 0) {
-            return 0;
-        }
-         if ( dp[n][target] != -1) {
+        if (dp[n][target] != -1) {
             return dp[n][target];
         }
-        if (target == 0 && n == 0) {
-            return 1;
+        
+        int ways = 0;
+        for (int i = 1; i <= k; i++) {
+            ways = (ways + rolls(n - 1, k, target - i, dp)) % (int)modulo;
         }
-        int res = 0;
-        if (target > 0  && n > 0) {
-            for (int i = 1; i <= k; i++) {
-                res += helper(n - 1, k, target - i, dp);
-                res = res % (int)MODULO;
-                dp[n][target] = res;
-            }
-        }
-        return dp[n][target] = res;
+        dp[n][target] = ways;
+        return ways;
     }
 }
