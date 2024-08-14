673a2becdc53cad1fa77ed07cbc3dc4883451ea7
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-08-14 08:52:25 -0400

    Time: 1 ms (94.27%), Space: 45.3 MB (35.83%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -11,23 +11,22 @@ class Solution {
                     int r = i;
                     int c = j;
                     int k = 1;
+                    boolean isCol = false;
                     while (i + k < m && board[i  + k][j] == 'X') {
                         board[i + k][j] = '.';
                         k++;
                     }
                     k = 1;
                     while (j + k < n  && board[i][j + k] == 'X') {
+                        isCol = true;
                         board[i][j + k] = '.';
                         k++;
                     }
+                    if (isCol)
+                        j = k ;
+                    
                 }
-            // for (int l = 0; l < m; l++) {
-            //     for (int t = 0; t< n; t++) {
-            //         System.out.println("hi");
-            //         System.out.println(board[l][t]);
-            //     }
-            // }
-        }
+            }
     }
         return battleShips;
     }
