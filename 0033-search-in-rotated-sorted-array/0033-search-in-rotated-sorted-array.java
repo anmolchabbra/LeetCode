5986965b2046fc67ba17c90608c17c2344e200ac
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-08-13 23:32:35 -0400

    Time: 14 ms (100.00%), Space: 42.9 MB (6.50%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,25 +1,26 @@
 class Solution {
     public int search(int[] arr, int target) {
-        int l = 0, r = arr.length - 1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (arr[m] == target) return m;
-            if (arr[l] <= arr[m]) {//left side sorted
-                if (target>= arr[l] && target <= arr[m]) {
-                    r = m - 1;
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            System.out.println("Start" + start + "end" + end);
+            int mid = start + (end - start)/ 2;
+            System.out.println("Mid " +mid);
+            if (arr[mid] == target) return mid;
+            //sorted
+            if (arr[start] <= arr[mid]) {
+                if (target >= arr[start] && target <= arr[mid]) {
+                    end = mid - 1;
                 } else {
-                    l = m + 1;
+                   start = mid + 1; 
                 }
-            } else {
-                //right is sorted
-                if (target> arr[m] && target <= arr[r]) {
-                    l = m + 1;
+            }else if (arr[mid] <= arr[end]) {
+                if (target >= arr[mid] && target <= arr[end]) {
+                    start = mid + 1;
                 } else {
-                    r = m - 1;
+                   end = mid - 1;
                 }
             }
         }
-        
         return -1;
     }
 }
