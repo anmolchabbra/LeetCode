5c7d40285ee5cd7c3766e99fed7e023263c059da
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2024-11-27 01:09:08 -0500

    Time: 6 ms (67.79%), Space: 44.2 MB (38.01%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,27 +1,46 @@
 class Solution {
     
-    Map<String, Integer> map = new HashMap<>();
+//     Map<String, Integer> map = new HashMap<>();
+    
+//     public int maxUncrossedLines(int[] nums1, int[] nums2) {
+//         int m = nums1.length;
+//         int n = nums2.length;
+//         return dfs(nums1, nums2, m , n);
+//     }
+    
+//     private int dfs(int[] a, int[] b, int m, int n) {
+//         if (m == 0 || n == 0) return 0;
+//         String key = m + "-" + n;
+//         //memo
+//         if (map.containsKey(key)) return map.get(key);
+//         if (a[m -1] == b[n - 1]) {
+//             int ret = 1 + dfs(a, b, m - 1, n - 1);
+//             map.put(key, ret);
+//             return ret;
+//         }
+//         int b1 = dfs(a, b, m - 1, n);
+//         int b2 = dfs(a, b, m, n - 1);
+//         int max = Math.max(b1, b2);
+//         map.put(key, max);
+//         return max;
+//     }
     
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        return dfs(nums1, nums2, m , n);
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
     
-    private int dfs(int[] a, int[] b, int m, int n) {
-        if (m == 0 || n == 0) return 0;
-        String key = m + "-" + n;
-        //memo
-        if (map.containsKey(key)) return map.get(key);
-        if (a[m -1] == b[n - 1]) {
-            int ret = 1 + dfs(a, b, m - 1, n - 1);
-            map.put(key, ret);
-            return ret;
-        }
-        int b1 = dfs(a, b, m - 1, n);
-        int b2 = dfs(a, b, m, n - 1);
-        int max = Math.max(b1, b2);
-        map.put(key, max);
-        return max;
-    }
 }
